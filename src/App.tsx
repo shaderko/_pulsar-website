f18c06fa6d3319bbe2fac247fc152f7542e95293
Filip Lukovic <shader228@gmail.com>
2023-11-27 18:23:00 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ height: '150vh' }}></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect, useRef } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [hovered, setHovered] = useState<boolean>(false);
+  const [scrolled, setScrolled] = useState<number>(0);
+  const [open, setOpen] = useState<boolean>(true);
+
+  const mainNavbarRef = useRef<HTMLDivElement>(null);
+  const hoverRef = useRef<HTMLDivElement>(null);
+
+  const scrollHandler = () => {
+    setScrolled(window.scrollY);
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+
+    hoverRef.current!.addEventListener('mouseover', () => {
+      setHovered(true);
+    });
+    hoverRef.current!.addEventListener('mouseleave', () => {
+      setHovered(false);
+    });
+
+    mainNavbarRef.current!.classList.remove('loading');
+
+    // create the hover box
+
+    return () => {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!hovered && scrolled >= 40) {
+      setOpen(false);
+      return;
+    }
+    setOpen(true);
+  }, [hovered, scrolled]);
+
+  useEffect(() => {
+    if (open) {
+      mainNavbarRef.current?.classList.remove('closed');
+    } else {
+      mainNavbarRef.current?.classList.add('closed');
+    }
+  }, [open]);
+
+  return (
+    <>
+      <div ref={hoverRef} className='navbar-hover'>
+        <div ref={mainNavbarRef} className='navbar closed loading'>
+          <ul className='navbar-menu'>
+            <li className='navbar-item active'>
+              <a>
+                <img src='./pulsar_logo.png'></img>
+              </a>
+            </li>
+            <li className='navbar-item'>
+              <a>About us</a>
+            </li>
+            <li className='navbar-item'>
+              <a>Projects</a>
+            </li>
+            <li className='navbar-item'>
+              <a>Contact us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
